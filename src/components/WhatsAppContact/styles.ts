8a4008b64f73b7f66bbfad1933febc9a31a10415
Felipe De Andrade <felipe.deam98@gmail.com>
2026-05-20 09:18:22 -0300

    refactor(component): adjust border spacing WhatsappContact

--- src/components/WhatsAppContact/styles.ts
+++ src/components/WhatsAppContact/styles.ts
@@ -2,6 +2,11 @@ import styled from "styled-components";
 
 export const WhatsappContactStyled = styled.a`
   position: fixed;
-  bottom: 4rem;
-  right: 4rem;
+  bottom: 2rem;
+  right: 2rem;
+
+  @media screen and (min-width: 1024px){
+    bottom: 4rem;
+    right: 4rem;
+  }
 `
